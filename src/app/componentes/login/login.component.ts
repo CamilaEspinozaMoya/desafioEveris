8e26714d047cdc2cdfb75c95c1ecfd991bb8b5a4
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-10-24 12:17:42 -0300

    material angular

---
+++ src/app/componentes/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router} from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
