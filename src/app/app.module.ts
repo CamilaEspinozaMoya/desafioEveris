e3aa731dc62c2d5e263f444e34b9cb28e0e0d917
CamilaEspinozaMoya <camila.andrea.espinoza@gmail.com>
2018-10-24 14:18:44 -0300

    avance login

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,20 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LoginComponent } from './componentes/login/login.component';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { RouterModule, Routes } from '@angular/router';
 import { ComentariosComponent } from './componentes/comentarios/comentarios.component';
-
-const appRoutes: Routes = [
-  {
-    path: 'login',
-    component: LoginComponent
-  },
-];
-
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AngularFireModule } from '@angular/fire';
 import { environment } from '../environments/environment';
