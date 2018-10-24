f4033f040e294c2ac67b0505b1f12c45f260f022
camila <camiramirezpp@gmail.com>
2018-10-24 13:15:57 -0300

    splash en proceso

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,21 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SplashComponent } from './splash/splash.component';
+import { Routes } from '@angular/router';
+
+const appRoutes: Routes = [
+  {
+    path: 'splash',
+    component: SplashComponent,
+
+  }
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SplashComponent
   ],
   imports: [
     BrowserModule,
@@ -16,3 +27,4 @@ import { AppComponent } from './app.component';
   bootstrap: [AppComponent]
 })
 export class AppModule { }
+
