f222c0953268eb4b287f792f44fd8712f0baed86
sorayasm <soraya.sacaan@gmail.com>
2018-10-25 12:58:46 -0300

    amazon

--- src/app/services/amazon.service.ts
+++ src/app/services/amazon.service.ts
@@ -14,7 +14,7 @@ public url: string;
 
   public getAmazon(title) {
     this.replace = title.replace(' ', '-');
-    this.url = `https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Dinstant-video&field-keywords=${this.replace}`;
+    this.url = `https://www.amazon.com/s/ref=nb_sb_noss?url=search-alias%3Dmovies-tv-intl-ship&field-keywords=${this.replace}`;
     console.log(this.url);
   }
 }
