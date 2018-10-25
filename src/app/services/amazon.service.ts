1880e8dec04381489958c523af74387078fefbcb
sorayasm <soraya.sacaan@gmail.com>
2018-10-25 12:44:52 -0300

    merge

--- src/app/services/amazon.service.ts
+++ src/app/services/amazon.service.ts
@@ -14,7 +14,7 @@ public url: string;
 
   public getAmazon(title) {
     this.replace = title.replace(' ', '-');
-    this.url = `https://www.amazon.com/${this.replace}/dp/B07DQMJBHS/`;
+    this.url = `https://www.amazon.com/s/ref=nb_sb_noss_1?url=search-alias%3Dinstant-video&field-keywords=${this.replace}`;
     console.log(this.url);
   }
 }
